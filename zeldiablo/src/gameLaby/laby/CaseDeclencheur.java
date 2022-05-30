5a154a817d8536428c14314fd453706596c33636
gretter6 <guillaume.retter5@etu.univ-lorraine.fr>
2022-05-30 17:18:12 +0200

    implémentation Case

--- zeldiablo/src/gameLaby/laby/CaseDeclencheur.java
+++ zeldiablo/src/gameLaby/laby/CaseDeclencheur.java
@@ -1,7 +1,15 @@
 package gameLaby.laby;
 
-public class CaseDeclencheur {
-    public void faireEffet(){
+public class CaseDeclencheur extends Case {
+
+    public CaseDeclencheur(int x, int y) {
+        super(x, y);
 
     }
+
+    public void faireEffet(){
+        System.out.println("case déclenchéée");
+    }
+
+
 }
